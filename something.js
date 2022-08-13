6b89e7864bb31000bad48dc7798e38839c92ec7d
AJS0191 <ashtonjamess@live.com>
2022-08-13 08:10:35 -0500

    improve nav bar; add caro section under hero

--- something.js
+++ something.js
@@ -2,11 +2,11 @@ const dropMenu = function () {
   const dropDown = document.getElementById('dropdown');
   const burgerDiv = document.getElementById('drop')
   console.log(burgerDiv);
-  if ($(dropDown).hasClass('invisible')) {
-    $(dropDown).removeClass('invisible');
+  if ($(dropDown).hasClass('no-display')) {
+    $(dropDown).removeClass('no-display');
     $(burgerDiv).addClass('clicked');
   } else {
-    $(dropDown).addClass('invisible');
+    $(dropDown).addClass('no-display');
     $(burgerDiv).removeClass('clicked');
   }
 }
