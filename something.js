37aec1b4e33ded17aa0286d1e2579a5c30062d75
Kaylee Dao <kayleed2@gmail.com>
2022-08-15 16:57:39 -0500

    All merged into main files

--- something.js
+++ something.js
@@ -1,19 +1,3 @@
-const dropMenu = function () {
-  const dropDown = document.getElementById('dropdown');
-  const burgerDiv = document.getElementById('drop')
-  const burgerSpan = document.getElementById('spanburger');
-  console.log(burgerDiv);
-  if ($(dropDown).hasClass('no-display')) {
-    $(dropDown).removeClass('no-display');
-    $(burgerDiv).addClass('clicked');
-    $(burgerSpan).removeClass('ml-burger');
-  } else {
-    $(dropDown).addClass('no-display');
-    $(burgerDiv).removeClass('clicked');
-    $(burgerSpan).addClass('ml-burger');
-  }
-}
-
 $(document).ready(function() {
   // executes when HTML-Document is loaded and DOM is ready
    
@@ -50,5 +34,28 @@ $( "#startbtn" ).hover(
     $(this).removeClass('shadow-lg');
   }
 );
+
+$('submit-btn').click(saveFile());
+
  });
 
+const dropMenu = function () {
+  const dropDown = document.getElementById('dropdown');
+  const burgerDiv = document.getElementById('drop')
+  const burgerSpan = document.getElementById('spanburger');
+  console.log(burgerDiv);
+  if ($(dropDown).hasClass('no-display')) {
+    $(dropDown).removeClass('no-display');
+    $(burgerDiv).addClass('clicked');
+    $(burgerSpan).removeClass('ml-burger');
+  } else {
+    $(dropDown).addClass('no-display');
+    $(burgerDiv).removeClass('clicked');
+    $(burgerSpan).addClass('ml-burger');
+  }
+}
+
+const saveFile = function () {
+  let inputs = $('input[type=email').val();
+  console.log(inputs);
+}
